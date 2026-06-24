089f9191be17a505e0bdd25088b08ab2c0943ca0
Teja0118 <157273408+Teja0118@users.noreply.github.com>
2026-06-24 19:07:42 +0530

    Work on Prediction Issue

--- app/api/services/prediction_service.py
+++ app/api/services/prediction_service.py
@@ -1,4 +1,124 @@
+import joblib
+
+from api.services.inference_preprocessor import (
+    InferencePreprocessor
+)
+
+
+class PredictionService:
+
+    def __init__(self):
+
+        self.model = joblib.load(
+            "models/pca_kmeans_model.pkl"
+        )
+
+        self.preprocessor = (
+            InferencePreprocessor()
+        )
+
+    def predict_cluster(
+        self,
+        guest_data: dict
+    ):
+
+        try:
+
+            processed_dataframe = (
+
+                self.preprocessor.preprocess_input(
+                    guest_data
+                )
+            )
+
+            cluster_prediction = (
+
+                self.model.predict(
+                    processed_dataframe
+                )[0]
+            )
+
+            print(
+                "\n========================"
+            )
+
+            print(
+                f"Predicted Cluster ID: "
+                f"{cluster_prediction}"
+            )
+
+            distances = (
+
+                self.model.transform(
+                    processed_dataframe
+                )[0]
+            )
+
+            print(
+                f"Distances: {distances}"
+            )
+
+            print(
+                "========================\n"
+            )
+
+            distances = (
+
+                self.model.transform(
+                    processed_dataframe
+                )[0]
+            )
+
+            sorted_distances = sorted(
+                distances
+            )
+
+            nearest_distance = (
+                sorted_distances[0]
+            )
+
+            second_nearest_distance = (
+                sorted_distances[1]
+            )
+
+            confidence = round(
+
+                (
+                    second_nearest_distance
+                    /
+                    (
+                        nearest_distance
+                        +
+                        second_nearest_distance
+                    )
+                ) * 100,
+
+                2
+            )
+
+            return {
+
+                "cluster_id":
+                    int(
+                        cluster_prediction
+                    ),
+
+                "cluster_confidence":
+                    confidence
+            }
+
+        except Exception as error:
+
+            raise Exception(
+
+                f"Error during cluster "
+                f"prediction: {error}"
+            )
+
+
 '''
+#v1
+
 import joblib
 
 from api.services.inference_preprocessor import (
@@ -92,7 +212,8 @@ class PredictionService:
                 f"prediction: {error}"
             )
 
-'''
+
+#vr2
 import joblib
 
 from api.services.inference_preprocessor import (
@@ -222,8 +343,7 @@ class PredictionService:
                 f"prediction: {error}"
             )
 
-'''
-
+#v3
     def predict_cluster(
         self,
         guest_data: dict
